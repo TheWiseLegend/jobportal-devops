820a596c152fe49f9af5baccf886ba490a0f28c9
Amr Ibrahim <amribrahim.dev@gmail.com>
2026-05-01 15:55:01 +0300

    feat: restructure the shared library

---
+++ jenkins/shared-library/vars/pipelines.groovy
@@ -0,0 +1,11 @@
+// Entry point for all pipeline templates.
+// Called directly from each app's Jenkinsfile.
+// Routes to the correct flow based on the current Git branch.
+
+def javaApp(Map params = [:]) {
+    if (env.BRANCH_NAME == 'main') {
+        new pipelines.javaApp.JavaAppMain(this).run(params)
+    } else {
+        new pipelines.javaApp.JavaAppFeature(this).run(params)
+    }
+}
